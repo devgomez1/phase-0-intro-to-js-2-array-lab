3106c528449c12ce63bd01abc5bce615adb86f48
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-10-10 12:24:27 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    console.log(cats);
+};
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    console.log(cats);
+};
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    console.log(cats);
+};
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    console.log(cats);
+};
+
+function appendCat(name){
+    return [...cats, name];
+};
+
+function prependCat(name){
+    return [name, ...cats];
+};
+
+function removeLastCat(name){
+    return cats.slice(0, -1);
+};
+
+function removeFirstCat(name){
+    return cats.slice(1);
+};
